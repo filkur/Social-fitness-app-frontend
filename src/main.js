d9703451f30609b405a0a2d49089eccb4020f9f6
filkur <filipkuranda@gmail.com>
2021-10-30 18:26:51 +0200

    add axios to connect with api

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
 
 
 Vue.config.productionTip = false
