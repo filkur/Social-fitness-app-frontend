8396be0a628df44e8ebc6904ee99f7d0dc757657
filkur <filipkuranda@gmail.com>
2021-10-30 14:18:04 +0200

    start project

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,22 @@
+<template>
+<div class = "register">
+  <div>
+    <navbar></navbar>
+  </div>
+</div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar/Navbar'
+
+export default {
+  name: "Register",
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
