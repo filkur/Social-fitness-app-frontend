fe9b1b5448dda3cafee39b0c6ba17a3014d1e414
filkur <filipkuranda@gmail.com>
2021-10-30 19:50:26 +0200

    create reqest

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -2,7 +2,7 @@
   <div class="container border bg-light rounded" style="margin-top: 150px; width: 450px;">
     <div class="form mt-3">
       <div class="d-flex justify-content-center mb-3">
-        <h3>Wypełnij formularz:</h3>
+        <h3>Dołącz do nas:</h3>
       </div>
       <div class="form-group">
         <input v-model="user.email" class="form-control" type="email" id="email" name="email" placeholder="E-mail">
@@ -14,7 +14,7 @@
         <input v-model="user.password" class="form-control" type="password" id="password" name="password" placeholder="Hasło">
       </div>
       <div class="form-group">
-        <button @click="saveCategory" class="btn btn-primary btn-block">Zarejestruj się</button>
+        <button @click="saveUser" class="btn btn-primary btn-block">Zarejestruj się</button>
       </div>
       <hr>
       <div class=" d-flex justify-content-center">
@@ -25,6 +25,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Register",
   data () {
@@ -34,6 +36,22 @@ export default {
         nickname: '',
         password:'',
       }
+    }
+  },
+  methods: {
+    saveUser(){
+      axios.post(
+          'http://localhost:8000/api/user/register',
+          JSON.stringify(this.user),
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+      )
+      .then((response) => {
+        console.log(response.data)
+      })
     }
   }
 }
