eb33fed1d0bca20a7f58840fef003adda8810563
filkur <filipkuranda@gmail.com>
2021-11-06 14:32:28 +0100

    add group view

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -1,38 +1,90 @@
 <template>
   <div class="containe">
     <navbar></navbar>
-    <div>lala</div>
+    <div class="container" style="margin-top: 50px;">
+      <div class="d-flex justify-content-start  mt-4">
+        <h2>Groups:</h2>
+      </div>
+      <div class="container rounded mt-4">
+        <table class="table table-striped">
+          <thead >
+            <tr>
+              <td>Name</td>
+              <td>Description</td>
+              <td>Owner</td>
+              <td>Created At</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+          <tr v-for="group in groups" v-bind:key="group.id">
+            <td>{{group.name}}</td>
+            <td>{{group.description}}</td>
+            <td>{{group.owner.nickname}}</td>
+            <td>{{group.createdAt}}</td>
+            <td>
+              <button @click="groupDetails(group.id)" class="btn btn-success" type="submit">Details</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar/Navbar'
+import axios from "axios";
 
 export default {
   components: {
     Navbar
   },
   name: "Group",
-  data () {
+  data() {
     return {
-      token : '',
+      token: '',
       userId: '',
       email: '',
       nickname: '',
+      groups:undefined
     }
   },
   mounted() {
-    if (localStorage.token){
+    if (localStorage.token) {
       this.token = localStorage.token;
     }
-    if (localStorage.id){
+    if (localStorage.id) {
       this.userId = localStorage.id;
     }
-    if (localStorage.email){
+    if (localStorage.email) {
       this.email = localStorage.email;
     }
-    if (localStorage.nickname){
+    if (localStorage.nickname) {
       this.nickname = localStorage.nickname;
+    }
+    this.fetchGroups();
+  },
+  methods: {
+    fetchGroups() {
+      axios.get(
+          'http://localhost:8000/api/group',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.token}`
+            }
+          }
+      )
+          .then((resp) => {
+            console.log(resp.data)
+            this.groups = resp.data;
+          })
+    },
+    groupDetails(id) {
+      this.$router.push('group/'+id)
     }
   }
 }
