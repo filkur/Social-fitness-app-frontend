8787ed25b02cd22bc721272a125f47161a39b870
filkur <filipkuranda@gmail.com>
2021-11-07 16:55:48 +0100

    add new activity

--- src/views/Event/EventDetails.vue
+++ src/views/Event/EventDetails.vue
@@ -6,7 +6,27 @@
         <div class="row d-flex justify-content-center mb-4">
           <h2>Results</h2>
         </div>
-        <div v-for="(member, index) in event.eventMembers" v-bind:key="member.id" class="container border bg-light rounded mt-5" style="max-width: 700px;">
+        <div class="container mt-2" style="max-width: 700px;">
+          <div>
+            <b-button v-b-toggle.collapse-1 variant="secondary">Add new activity</b-button>
+            <b-collapse id="collapse-1" class="mt-2">
+              <b-card>
+                <div class="form">
+                  <div class="form-group">
+                    <input v-model="newActivity.name" class="form-control" type="text" id="name" name="name" placeholder="name here" required>
+                  </div>
+                  <div class="form-group">
+                    <input v-model="newActivity.value" class="form-control" type="text" id="value" name="value" placeholder="value here" required>
+                  </div>
+                  <div class="form-group">
+                    <button @click="addActivity()" class="btn btn-success btn-block">add</button>
+                  </div>
+                </div>
+              </b-card>
+            </b-collapse>
+          </div>
+        </div>
+        <div v-for="(member, index) in event.eventMembers" v-bind:key="member.id" class="container border bg-light rounded mt-3" style="max-width: 700px;">
           <div class="d-flex justify-content-around">
             <div class="p-2">Place: {{index+1}}</div>
             <div class="p-2">Nickname: {{member.user.nickname}}</div>
@@ -61,7 +81,11 @@ export default {
       token: localStorage.token,
       loggedUserId: localStorage.id,
       event:undefined,
-      eventMember: undefined
+      newActivity: {
+        eventId: this.$route.params.id,
+        name: '',
+        value: ''
+      }
     }
   },
   mounted() {
@@ -95,6 +119,28 @@ export default {
       .then((res) => {
         location.reload();
       })
+    },
+    addActivity(){
+      this.newActivity.value = parseInt(this.newActivity.value)
+      axios.post(
+          'http://localhost:8000/api/activity',
+          JSON.stringify(this.newActivity),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.token}`
+            }
+          }
+      )
+          .then((response) => {
+            alert('Added new activity')
+            location.reload();
+          })
+          .catch(function (error){
+            if (error.response){
+              alert(error.response.data["hydra:description"]);
+            }
+          })
     }
   }
 }
