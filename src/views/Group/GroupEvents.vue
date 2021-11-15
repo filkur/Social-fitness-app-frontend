7d06ac9f921f6a73051cccb910c52f743700fd19
filkur <filipkuranda@gmail.com>
2021-11-15 20:23:20 +0100

    add event feature

--- src/views/Group/GroupEvents.vue
+++ src/views/Group/GroupEvents.vue
@@ -6,6 +6,48 @@
     <div class="row d-flex justify-content-start mt-5">
       <h2>Group Events:</h2>
     </div>
+    <div>
+      <b-button v-b-modal.modal-1>Add new event</b-button>
+
+      <b-modal
+          id="modal-1"
+          title="New Event:"
+          @ok="createEvent()"
+      >
+        <div class="form">
+          <div class="form-group">
+            <input v-model="newEvent.name" class="form-control" type="text" id="name" name="name" placeholder="name" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.description" class="form-control" type="text" id="description" name="description" placeholder="description here" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.pointGoal" class="form-control" type="number" min="0" id="pointGoal" name="pointGoal" placeholder="pointGoal" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.pointPerRep" class="form-control" type="number" min="0" id="pointPerRep" name="pointPerRep" placeholder="pointPerRep" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.pointPerMinute" class="form-control" type="number" min="0" id="pointPerMinute" name="pointPerMinute" placeholder="pointPerMinute" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.startDate" class="form-control" type="date" id="startDate" name="startDate" placeholder="startDate" required>
+          </div>
+          <div class="form-group">
+            <input v-model="newEvent.endDate" class="form-control" type="date" id="endDate" name="endDate" placeholder="endDate" required>
+          </div>
+          <div class="form-group">
+            <select v-model="newEvent.eventType">
+              <option disabled value="">Type:</option>
+              <option>TIME</option>
+              <option>TIME_LESS</option>
+              <option>REP</option>
+            </select>
+          </div>
+        </div>
+      </b-modal>
+
+    </div>
     <div v-for="event in group.events" v-bind:key="event.id" class="container border bg-light rounded mt-5 mb-5 p-4">
       <div class="row">
         <div class="col">
@@ -47,6 +89,17 @@ export default {
       token: localStorage.token,
       loggedUserId: localStorage.id,
       group:undefined,
+      newEvent:{
+        groupId: this.$route.params.id,
+        name: '',
+        description: '',
+        pointGoal: '',
+        pointPerRep: '',
+        pointPerMinute: '',
+        startDate: '',
+        endDate: '',
+        eventType: ''
+      }
     }
   },
   mounted() {
@@ -66,6 +119,30 @@ export default {
   methods: {
     eventDetails(id){
       this.$router.push('eventDetails');
+    },
+    createEvent(){
+      this.newEvent.pointGoal = parseInt(this.newEvent.pointGoal);
+      this.newEvent.pointPerRep = parseInt(this.newEvent.pointPerRep);
+      this.newEvent.pointPerMinute = parseInt(this.newEvent.pointPerMinute);
+      axios.post(
+          'http://localhost:8000/api/event',
+          JSON.stringify(this.newEvent),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${this.token}`
+            }
+          }
+      )
+          .then((response) => {
+            alert('Added new Event')
+            location.reload();
+          })
+          .catch(function (error){
+            if (error.response){
+              alert(error.response.data["hydra:description"]);
+            }
+          })
     }
   }
 }
